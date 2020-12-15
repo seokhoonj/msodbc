94a4522eebc686a36440d6fe95f82095b58ad542
seokhoonj <seokhoonj@gmail.com>
2020-12-15 15:50:50 +0900

    unchar -> trimws

--- R/etc.R
+++ R/etc.R
@@ -2,6 +2,6 @@ clean <- function(df) {
   col_list <- names(sapply(df, class)[sapply(df, class) == "list"])
   col_char <- names(sapply(df, class)[sapply(df, class) == "character"])
   df[, col_list] <- lapply(df[, col_list], unlist)
-  df[, col_char] <- lapply(df[, col_char], unchar)
+  df[, col_char] <- lapply(df[, col_char], trimws)
   df
 }
